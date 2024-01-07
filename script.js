cf87ec4782cde839902a5aaa9fbe2822f0925131
nsx_69 <mrmanafest16@gmail.com>
2024-01-07 17:46:00 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// console.log(123);
+
+// let num = 123;
+// console.log(num);
+
+// let num1 = 123;
+// let num2 = 456;
+// console.log(num1);
+// console.log(num2);
+
+// let num1 = 123;
+// let num2 = 456;
+// let num3 = 789
+// console.log(num1, num2, num3);
+
+// console.log(123);
+// console.log('Hello world!');
+// console.log(true);
+
+//alert(eee); // Uncaught ReferenceError: eee is not defined
+
+// const PI = 3.14;
+// console.log(PI * (3/2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
